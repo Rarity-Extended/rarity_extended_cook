cb0327897362cd028035846d9b0aa38a5f1f73cc
Major-Eth <major-tom.eth@pm.me>
2022-01-26 13:45:03 +0100

    ✨ Adding deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,53 +1,48 @@
 import time
-from brownie import Cooking, Meal, Contract, accounts, convert
+from brownie import Cooking, accounts
 from ape_safe import ApeSafe
 
-CONTRACT_NAME = 'Rarity Extended Cooking'
-RARITY_EXTENDED_OP_MS = '0xFaEc40354d9F43A57b58Dc2b5cffe41564D18BB3'
-RARITY_EXTENDED_PROXY_DEPLOYER = '0x253aaEAFDA7AE3C6Ed3E3E2732C49cf077a22Ae0'
 RARITY_MANIFEST = '0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb'
-
 RARITY_GOLD = '0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2'
 RARITY_EXTENDED_LOOT_MUSHROOM = '0xcd80cE7E28fC9288e20b806ca53683a439041738'
 RARITY_EXTENDED_LOOT_BERRIES = '0x9d6C92CCa7d8936ade0976282B82F921F7C50696'
 RARITY_EXTENDED_LOOT_MEAT = '0x95174B2c7E08986eE44D65252E3323A782429809'
+RARITY_EXTENDED_OP_MS = '0xFaEc40354d9F43A57b58Dc2b5cffe41564D18BB3'
 
 safe = ApeSafe(RARITY_EXTENDED_OP_MS)
 
 # 🏹 - Rarity Extended #########################################################
 # This script is used to deploy the Rarity Extended Cooking contract from the
-# Rarity Extended Deployer, and set Extended as the operational multisig
-# address.
+# dev wallet, and then deploy some meals.
 #
-# Multisig address is 0xFaEc40354d9F43A57b58Dc2b5cffe41564D18BB3.
-# Deployer address is 0x253aaEAFDA7AE3C6Ed3E3E2732C49cf077a22Ae0
+# Cooking:							0x7474002fe5640d612c9a76cb0b6857932ff891e8
+# Meals:
+# - Grilled Meat					0x97e8f1061224cb532f808b074786c76e977ba6ee
+# - Mushroom soup					0x2e3e1c1f49a288ebf88be66a3ed3539b5971f25d
+# - Berries pie						0x57e4cd55289da26aa7cb607c00c5ddcd0f7980a2
+# - Mushroom and Meat Skewer		0x65567a2fbc14b4abcd414bb6902384745d4353f6
+# - Mushroom and Berries Mix		0xf06ffe67cb96641eec55ea19126bd8f0107ff0ad
+# - Berries Wine					0xa0e9159efc4407c4465bbcdf0e7538d6869d81a3
+#
+# Deployment cost: 5,1380111487 FTM
 ###############################################################################
 def deployWithDev():
-	# Use dev deployer account to deploy the contract, load it and setExtended to the Safe
 	deployer = accounts.load('rarityextended')
-	# cooking = deployer.deploy(Cooking, RARITY_MANIFEST)
-	cooking = Contract.from_explorer("0xf6157fb663768ca6147f89f7495a8ab895bc4c4b")
-	# cooking.setExtended(safe.account)
-
-	# Cooking.publish_source(cooking)
-
-	meal = Contract.from_abi('meal', "0xef28f159b3811d4AE7CAf5A55286265C053a33B6", Meal.abi)
-	Meal.publish_source(meal)
-	meal = Contract.from_abi('meal', "0x8cf1200204e81b8f1e610a2bfeeddf75e0d7bef0", Meal.abi)
-	Meal.publish_source(meal)
-
+	cooking = deployer.deploy(Cooking, RARITY_MANIFEST)
+	time.sleep(5)
+	Cooking.publish_source(cooking)
 
 	recipes = [
-		# ("Grilled Meat", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MEAT], [10, 10]),
-		# ("Mushroom soup", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MUSHROOM], [10, 10]),
+		("Grilled Meat", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MEAT], [10, 10]),
+		("Mushroom soup", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MUSHROOM], [10, 10]),
 		("Berries pie", [RARITY_GOLD, RARITY_EXTENDED_LOOT_BERRIES], [10, 10]),
-		# ("Mushroom and Meat Skewer", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MEAT, RARITY_EXTENDED_LOOT_MUSHROOM], [10, 10, 10]),
-		# ("Mushroom and Berries Mix", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MUSHROOM, RARITY_EXTENDED_LOOT_BERRIES], [10, 10, 10]),
-		# ("Berries Wine", [RARITY_GOLD, RARITY_EXTENDED_LOOT_BERRIES], [50, 100]),
+		("Mushroom and Meat Skewer", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MEAT, RARITY_EXTENDED_LOOT_MUSHROOM], [10, 10, 10]),
+		("Mushroom and Berries Mix", [RARITY_GOLD, RARITY_EXTENDED_LOOT_MUSHROOM, RARITY_EXTENDED_LOOT_BERRIES], [10, 10, 10]),
+		("Berries Wine", [RARITY_GOLD, RARITY_EXTENDED_LOOT_BERRIES], [50, 100]),
 	]
 
 	for (name, ingredients, quantities) in recipes:
-		tx = cooking.createNewRecipe(
+		cooking.createNewRecipe(
 			name,
 			name + ' - (Meal)',
 			'',
@@ -55,21 +50,7 @@ def deployWithDev():
 			quantities,
 			{"from": deployer}
 		)
-		deployedMeal = tx.events['createdNewRecipe'][0]['addr'];
-		Contract.from_abi(name, deployedMeal, Meal.abi)
 		time.sleep(5)
-		Meal.publish_source(deployedMeal)
-		
-
-	# # Send transaction to mutlisig
-	# safe_tx = safe.multisend_from_receipts()
-	# safe.preview(safe_tx)
-	# safe.post_transaction(safe_tx)
-
 
 def main():
 	deployWithDev()
-	# addRecipes(safe_cook)
-
-
-# {'RoleGranted': [OrderedDict([('role', 0x0000000000000000000000000000000000000000000000000000000000000000), ('account', '0xF6157FB663768ca6147F89F7495A8ab895BC4C4b'),('sender', '0xF6157FB663768ca6147F89F7495A8ab895BC4C4b')]), OrderedDict([('role', 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6), ('account', '0xF6157FB663768ca6147F89F7495A8ab895BC4C4b'), ('sender', '0xF6157FB663768ca6147F89F7495A8ab895BC4C4b')])], 'createdNewRecipe': [OrderedDict([('addr', '0xef28f159b3811d4AE7CAf5A55286265C053a33B6'), ('name', 'Mushroom soup'), ('symbol', 'Mushroom soup - (Meal)'), ('effect', ''), ('ingredients', ('0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2', '0xcd80cE7E28fC9288e20b806ca53683a439041738')), ('quantities', (10, 10))])]}
